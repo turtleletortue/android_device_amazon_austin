c0cb5b1167ae6a67fd252fc8a75dbfbc9eb866fc
ggow <joel.bacchus@protonmail.com>
2019-04-24 20:18:12 +0100

    Initial commit

---
+++ full_austin.mk
@@ -0,0 +1,13 @@
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/amazon/austin/device.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_DEVICE := austin
+PRODUCT_NAME := full_austin
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Fire
+PRODUCT_MANUFACTURER := amzn
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
